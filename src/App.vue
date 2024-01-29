7eb0a843da2b7a8b9abe728380450d172ecbd902
qiang <guoshengqiang@live.com>
2024-01-30 02:41:05 +0800

    fix: code use highlight

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
 import { onMounted, ref, nextTick, reactive } from 'vue'
 import { onDidReceiveMessage, postMessage, onReady } from './vscode'
-import * as marked from 'marked'
+import { Marked } from 'marked'
 import { throttle } from 'lodash-es'
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js/lib/common'
+// import 'highlight.js/styles/github.css'
+// import 'highlight.js/styles/github-dark.css'
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    }
+  })
+)
 
 type Line = {
   end?: boolean
@@ -86,13 +100,7 @@ onMounted(() => {
     <div v-for="(item, index) in list" :key="index" v-html="item.html"></div>
   </div>
   <div class="input-box">
-    <textarea
-      type="text"
-      placeholder="询问 Copilot"
-      rows="1"
-      v-model="textarea"
-      @keydown.enter="enter"
-    ></textarea>
+    <textarea type="text" placeholder="询问 Copilot" rows="1" v-model="textarea" @keydown.enter="enter"></textarea>
     <button @click="send">▷</button>
   </div>
 </template>
@@ -114,8 +122,8 @@ body {
   flex: 1;
   /* user-select: none; */
   box-sizing: border-box;
-  background-color: var(--vscode-editor-background);
-  color: var(--vscode-editor-foreground);
+  background-color: var(--vscode-background);
+  color: var(--vscode-foreground);
 }
 
 .session {
@@ -175,5 +183,16 @@ button {
   top: 50%;
   margin-top: -11px;
   color: var(--vscode-input-foreground);
+}
+
+.hljs {
+  background-color: var(--vscode-editor-background);
+  color: var(--vscode-editor-foreground);
+  display: block;
+  padding: 5px;
+  padding-bottom: 15px;
+  overflow-y: auto;
+  border-radius: 3px;
+  border: 1px solid rgba(127, 127, 127, 0.5);
 }
 </style>
